4e70553e1c3dc0107d6ead6b1c14b07589d1b57c
samantha copeland <ninianne98@gmail.com>
2024-05-24 19:37:29 -0500

    bootstrap 5 work

--- Northwind/Models/SkinItem.cs
+++ Northwind/Models/SkinItem.cs
@@ -1,23 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using static Carrotware.Web.UI.Components.Bootstrap;
+﻿using static Carrotware.Web.UI.Components.Bootstrap;
+using static Carrotware.Web.UI.Components.Bootstrap5;
 
 namespace Northwind.Models {
 
-	public class SkinItem {
+	public class Skin3Item {
 
-		public SkinItem() {
+		public Skin3Item() {
 			this.Skin = BootstrapColorScheme.NotUsed;
 			this.SkinName = this.Skin.ToString();
 		}
 
-		public SkinItem(BootstrapColorScheme skin) {
+		public Skin3Item(BootstrapColorScheme skin) {
 			this.Skin = skin;
 			this.SkinName = this.Skin.ToString();
 		}
 
 		public BootstrapColorScheme Skin { get; set; }
+		public string SkinName { get; set; }
+	}
+
+	//=============================
+
+	public class Skin5Item {
+
+		public Skin5Item() {
+			this.Skin = Bootstrap5ColorScheme.NotUsed;
+			this.SkinName = this.Skin.ToString();
+		}
+
+		public Skin5Item(Bootstrap5ColorScheme skin) {
+			this.Skin = skin;
+			this.SkinName = this.Skin.ToString();
+		}
+
+		public Bootstrap5ColorScheme Skin { get; set; }
 		public string SkinName { get; set; }
 	}
 }
